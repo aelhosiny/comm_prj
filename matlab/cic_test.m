d7c0b801a4c32207afcfd8c31d3573bac8c3f6fa
Amr Elhosiny <eng.amr2009@gmail.com>
2015-03-19 02:57:15 +0200

    First version of digcore, modified cic_test.m to run all samples in tdc_out

--- matlab/cic_test.m
+++ matlab/cic_test.m
@@ -6,7 +6,7 @@ fs_in = 625e6;
 ts_in = 1/625/1e6;
 simtime = 50000*ts_in;
 
-R = 16;
+R = 8;
 mux_sel = 3;
 
 data = csvread('output_156p.csv');
@@ -19,9 +19,10 @@ T = T'.*ts_in;
 % simtime = sum(T);
 
 TS_all = [T S];
-
 % TS = [ TS_all(1:1000,1) TS_all(1:1000,2)];
 TS = TS_all;
+
+simtime = (length(TS))*ts_in;
 
 if R==4
     SA =3;
@@ -51,11 +52,13 @@ dlmwrite('cic_out1.txt', cic_out_f1, '\r');
 % dlmwrite('sec2_out.txt', sec2_out_f, '\r');
 % dlmwrite('sec2_out1.txt', sec2_out1_f, '\r');
 
-% t1 = linspace(1,length(cic_in),length(cic_in));
-% figure
-% subplot(2,1,1) ; plot(t1,cic_in); grid on;
-% t1 = linspace(1,length(cic_out),length(cic_out));
-% subplot(2,1,2) ; plot(t1,cic_out); grid on;
-% 
-% plot_spectrum(cic_in, fs_in);
-% plot_spectrum(cic_out, fs_in/R);
+t1 = linspace(1,length(cic_in),length(cic_in));
+t1 = t1.*ts_in;
+figure
+subplot(2,1,1) ; plot(t1,cic_in); grid on;
+t1 = linspace(1,length(cic_out),length(cic_out));
+t1 = t1.*ts_in*R;
+subplot(2,1,2) ; plot(t1,cic_out); grid on;
+
+plot_spectrum(cic_in, fs_in);
+plot_spectrum(cic_out, fs_in/R);
---
+++ source/rtl/digcore.vhd
@@ -0,0 +1,85 @@
+-------------------------------------------------------------------------------
+-- Title      : digcore
+-- Project    : 
+-------------------------------------------------------------------------------
+-- File       : digcore.vhd
+-- Author     : amr  <amr@amr-laptop>
+-- Company    : 
+-- Created    : 18-03-2015
+-- Last update: 19-03-2015
+-- Platform   : RTL Compiler, Design Compiler, ModelSim, NC-Sim
+-- Standard   : VHDL'93
+-------------------------------------------------------------------------------
+-- Description: core digital operations such as DSP, clock generation
+-------------------------------------------------------------------------------
+-- Copyright (c) 2015 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 18-03-2015  1.0      amr     Created
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity digcore is
+  
+  port (
+    -- power on reset for the chip. Hard reset pin
+    por_rstn      : in std_logic;
+    -- soft reset to digcore from register file
+    digrf_rstn    : in std_logic;
+    -- enable/gate clock to digital from register file
+    enable_digclk : in std_logic;
+    -- input system clock, 625MHz clock input
+    sys_clk       : in std_logic;
+    -- decimator output valid signal, used to implement
+    -- programable divider clock
+    dec_vldout    : in std_logic;
+    -- decimation ratio, control freq of divided clock
+    dec_ratio     : in std_logic_vector(1 downto 0);
+    -- tdc output, input to digtop
+    tdc_out       : in std_logic_vector(4 downto 0)
+    );
+
+end entity digcore;
+
+architecture behav of digcore is
+
+  signal dig_rstn_s   : std_logic;      -- [out]
+  signal clk_625mhz_s : std_logic;      -- [out]
+  signal clk_lf_s     : std_logic;      -- [out]
+  signal cic_in_s     : std_logic_vector(5 downto 0);
+  signal cic_out_s    : std_logic_vector(9 downto 0);
+  signal dec_vldout_s : std_logic;
+  
+begin  -- architecture behav
+
+  cic_in_s <= '0' & tdc_out;
+
+
+  clk_rst_gen_1 : entity work.clk_rst_gen
+    port map (
+      por_rstn      => por_rstn,        -- [in  std_logic]
+      digrf_rstn    => digrf_rstn,      -- [in  std_logic]
+      enable_digclk => enable_digclk,   -- [in  std_logic]
+      sys_clk       => sys_clk,         -- [in  std_logic]
+      dec_vldout    => dec_vldout_s,    -- [in  std_logic]
+      dec_ratio     => dec_ratio,       -- [in  std_logic_vector(1 downto 0)]
+      dig_rstn      => dig_rstn_s,      -- [out std_logic]
+      clk_625mhz    => clk_625mhz_s,    -- [out std_logic]
+      clk_lf        => clk_lf_s);       -- [out std_logic]
+
+  decimation_1 : entity work.decimation
+    port map (
+      clk        => clk_625mhz_s,       -- [in  std_logic]
+      enable     => '1',                -- [in  std_logic]
+      rstn       => dig_rstn_s,         -- [in  std_logic]
+      dec_ratio  => dec_ratio,          -- [in  std_logic_vector(1 downto 0)]
+      filter_in  => cic_in_s,           -- [in  std_logic_vector(5 downto 0)]
+      filter_out => cic_out_s,          -- [out std_logic_vector(9 downto 0)]
+      ce_out     => dec_vldout_s);      -- [out std_logic]
+
+
+end architecture behav;
