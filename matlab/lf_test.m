d6aedc350f0a705c07cc9ab214139b159b06fd3e
Amr Elhosiny <eng.amr2009@gmail.com>
2015-03-20 23:12:04 +0200

    First version of IIR after quantization. Hopefully it works

---
+++ matlab/lf_test.m
@@ -0,0 +1,29 @@
+clear all;
+close all;
+clc;
+
+K1=122;
+K2=0.1328;
+K3=0.90625;
+
+ts_in = 1/625/1e6;
+R = 8;
+ts = R*ts_in;
+
+
+file_in = strcat('cic_out_',num2str(R),'.txt');
+lf_in = dlmread(file_in, '\r');
+
+t = linspace(1,length(lf_in), length(lf_in));
+t = t.*ts;
+t=t';
+
+lf_in_mat = [t lf_in];
+
+simtime = (length(lf_in_mat))*ts;
+
+sim('IIR1.mdl', simtime);
+
+% plot_spectrum(lf_in1, 1/ts);
+% plot_spectrum(lf_out, 1/ts);
+plot_spectrum(lf_out1, 1/ts);
